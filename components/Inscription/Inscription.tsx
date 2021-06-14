4bf516ea2580cd727a6b9d4cba7d2891604dfda7
FollowTheFlo <florent.letendre@hotmail.fr>
2021-06-14 17:31:43 -0400

    fix FormSubmit event sending previous value

--- components/Inscription/Inscription.tsx
+++ components/Inscription/Inscription.tsx
@@ -3,23 +3,37 @@ import { Button, Checkbox, Form, FormLayout, TextField } from '@shopify/polaris'
 import axios from 'axios';
 import React, { useEffect, useState, useCallback, useContext } from 'react';
 import { SuccessResponse } from '../../model/responses.model';
+import AdminContext from '../../store/admin-context';
 
 const Inscription:React.FC = (props) => {
     const [newsletter, setNewsletter] = useState(false);
     const [code, setCode] = useState('');
+    const ctx = useContext(AdminContext);
   
-    const handleSubmit = useCallback((_event) => {
+    const handleSubmit = useCallback((e) => {
+      e.preventDefault();
       console.log('handleSubmit1', code);
-      if(!code || code === '') {
+      const tempCode = e.target.codeField.value;
+      console.log('handleSubmit2', tempCode);
+      if(!tempCode || tempCode === '') {
         console.log('Please enter code');
         return;
       }
-        console.log('handleSubmit', code);
-      axios.post('/api/integration/activate',{code:code})
+    
+     // setCode(tempCode);
+      
+        console.log('handleSubmit', tempCode);
+      axios.post('/api/integration/activate',{code:tempCode})
       .then(response => {
         const result = response?.data as SuccessResponse;
+        console.log('response integration api', result);
 
-        console.log('response integration api', result);       
+        if(!result || !result.success) {
+          console.log('problem while activation')
+          return;
+        }
+        
+        ctx.onIntegrationChange(true);
                   
       })
       .catch(err => {
@@ -44,6 +58,7 @@ const Inscription:React.FC = (props) => {
           /> */}
   
           <TextField
+            id="codeField"
             value={code}
             onChange={handleCodeChange}
             label="Api code"
