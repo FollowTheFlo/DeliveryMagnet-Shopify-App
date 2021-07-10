e66052022390985803fa2e5b84fb20035987c18d
FollowTheFlo <florent.letendre@hotmail.fr>
2021-07-10 02:04:54 -0400

    add Inscriptions steps and design

--- components/Inscription/Inscription.tsx
+++ components/Inscription/Inscription.tsx
@@ -1,5 +1,5 @@
 
-import { Button, Checkbox, Form, FormLayout, TextField } from '@shopify/polaris';
+import { Button, Card, Checkbox, DescriptionList, Form, FormLayout, Layout, List, TextField, TextStyle } from '@shopify/polaris';
 import axios from 'axios';
 import React, { useEffect, useState, useCallback, useContext } from 'react';
 import { SuccessResponse } from '../../model/responses.model';
@@ -49,30 +49,48 @@ const Inscription:React.FC = (props) => {
     }, []);
   
     return (
-      <Form onSubmit={handleSubmit}>
-        <FormLayout>
-          {/* <Checkbox
-            label="Sign up for the Polaris newsletter"
-            checked={newsletter}
-            onChange={handleNewsLetterChange}
-          /> */}
-  
-          <TextField
-            id="codeField"
-            value={code}
-            onChange={handleCodeChange}
-            label="Api code"
-            type="number"
-            helpText={
-              <span>
-                Generate Api code on RouteMagnet first.
-              </span>
-            }
-          />
-  
-          <Button submit>Integrate with RouteMagnet</Button>
-        </FormLayout>
-      </Form>
+      <Layout>
+      <Layout.Section >
+      <Card title="Activation">
+        <Card.Section>
+        <List type="bullet">
+          <List.Item><b>Step1:</b> Create a free RouteMagnet account on <a target="_blank" href={`${process.env.NEXT_PUBLIC_RM_CLIENT_URL}/register`}>Register</a></List.Item>
+          <List.Item><b>Step2:</b> Create a team on RouteMagnet <a target="_blank" href={`${process.env.NEXT_PUBLIC_RM_CLIENT_URL}/tabs/dashboard`}>Dashboard</a></List.Item>
+          <List.Item><b>Step3:</b> Generate API code on <a target="_blank" href={`${process.env.NEXT_PUBLIC_RM_CLIENT_URL}/tabs/integration`}>Integration page</a></List.Item>
+          <List.Item><b>Step4:</b> Copy and paste API code below, Press 'Connect with RouteMagnet'</List.Item>
+        </List>
+        
+<br/>
+          <Form onSubmit={handleSubmit}>
+            <FormLayout>
+              {/* <Checkbox
+                label="Sign up for the Polaris newsletter"
+                checked={newsletter}
+                onChange={handleNewsLetterChange}
+              /> */}
+      
+              <TextField
+                id="codeField"
+                value={code}
+                onChange={handleCodeChange}
+                label="API Code"
+                type='text'
+                clearButton
+                onClearButtonClick={() => setCode('')}               
+              />
+      
+              <Button 
+              disabled={!code}
+              submit>Connect with RouteMagnet</Button>
+            </FormLayout>
+          </Form>
+        </Card.Section>
+      </Card>
+      </Layout.Section>
+      <Layout.Section oneThird>
+
+      </Layout.Section>
+      </Layout>
     );
 }
 
