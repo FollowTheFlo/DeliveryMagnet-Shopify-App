7936a745fe41d4705ec0ac5bea1a2fcbdb522518
FollowTheFlo <florent.letendre@hotmail.fr>
2021-07-15 19:13:59 -0400

    mapping for string
    in futur for locales

--- components/utils/localMapping.ts
+++ components/utils/localMapping.ts
@@ -1,5 +1,9 @@
 const en = {
   COMPANY_NOT_FOUND: "Company not found",
+  CONFIG_NOT_FOUND: "Config not found",
+  ERROR_SAVING_CONFIG: "Error saving config",
+  ACTIVATION_NEEDED: "Activation needed",
+  SERVER_AUTHENTIFICATION_FAILED: "Server authentification failed",
 };
 
 export { en };
