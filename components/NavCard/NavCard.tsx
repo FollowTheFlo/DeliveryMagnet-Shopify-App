a80c57e0769b47e9bd044ebfd28dc6d4493a238b
FollowTheFlo <florent.letendre@hotmail.fr>
2021-07-15 19:12:12 -0400

    navigation Card in its own compnent

---
+++ components/NavCard/NavCard.tsx
@@ -0,0 +1,31 @@
+import { Button, ButtonGroup, Card } from "@shopify/polaris";
+import React from "react";
+import styles from "./NavCard.module.css";
+
+const NavCard = (props) => {
+  return (
+    <Card>
+      <Card.Section>
+        {/* <div style={{ verticalAlign: "middle" }}> */}
+        <ButtonGroup>
+          <Button
+            primary
+            disabled={!props.isIntegrated}
+            onClick={() => props.onClickpageSelection(0)}
+          >
+            Orders
+          </Button>
+          <Button
+            disabled={!props.isIntegrated}
+            onClick={() => props.onClickpageSelection(1)}
+          >
+            Settings
+          </Button>
+          <Button>FAQ - Support</Button>
+        </ButtonGroup>
+      </Card.Section>
+    </Card>
+  );
+};
+
+export default NavCard;
