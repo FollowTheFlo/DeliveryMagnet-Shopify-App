ba19c1293fceba63b5fb3893d668a46e9db021e5
FollowTheFlo <florent.letendre@hotmail.fr>
2021-07-15 00:49:45 -0400

    add IndexListSelection to fix Row Selection issue

--- model/input.model.ts
+++ model/input.model.ts
@@ -1,22 +1,28 @@
 import { JobOrder } from "./orders.model";
 
 export interface StatusAction {
-    status: string;
-    action: string;
+  status: string;
+  action: string;
 }
 
 export interface JobOrderProps {
-    order:JobOrder;
-    onPushOrder:(o:JobOrder)=>void;
-    onFulfillOrder:(o:JobOrder)=>void;
-  }
+  order: JobOrder;
+  onPushOrder: (o: JobOrder) => void;
+  onFulfillOrder: (o: JobOrder) => void;
+}
 
 export interface PageInfo {
-    hasNextPage:boolean;
-    hasPreviousPage:boolean;
-  }
+  hasNextPage: boolean;
+  hasPreviousPage: boolean;
+}
 
 export interface CursorSelection {
-    action:'before'|'after';
-    value:string;
+  action: "before" | "after";
+  value: string;
+}
+
+export interface IndexListSelection {
+  selectionType: "All" | "Multi" | "Page" | "Single";
+  toggleType: boolean;
+  selection: string[];
 }
