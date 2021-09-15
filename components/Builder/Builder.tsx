d1e95ad5235fbf4d5ff3ad0ac109cfcd041a5a79
FollowTheFlo <florent.letendre@hotmail.fr>
2021-09-14 22:51:44 -0400

    iframe height bigger

--- components/Builder/Builder.tsx
+++ components/Builder/Builder.tsx
@@ -9,7 +9,7 @@ const Builder: React.FC = (props) => {
       <iframe
         frameBorder="0"
         width="100%"
-        height="800px"
+        height="1280px"
         src={`${process.env.NEXT_PUBLIC_RM_CLIENT_URL}`}
       ></iframe>
     </React.Fragment>
